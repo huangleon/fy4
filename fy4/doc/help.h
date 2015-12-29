d9b2c7a2a293e72222fcc04e3b32a4e3c4b7ff2f
Huang,Liang <liang.huang@transmension.com>
2015-12-29 18:49:19 +0800

    Fix compiling error.
    Remove new line concatenator '\'

--- fy4/doc/help.h
+++ fy4/doc/help.h
@@ -1,8 +1,3 @@
  // This include file is included by the help command (/cmds/usr/help).
 // If you changed the structure of help docs, remember to update this file. 
-#define DEFAULT_SEARCH_PATHS ({\
-        "/doc/help/",\
-        "/doc/map/",\
-        "/doc/wiz/",\
-        "/doc/menpai/",\
-        })        
+#define DEFAULT_SEARCH_PATHS ({"/doc/help/", "/doc/map/", "/doc/wiz/", "/doc/menpai/",})
